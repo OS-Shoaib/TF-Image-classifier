320ee9cffc4fee445025e0dacf3cff3231bac7c5
Omar Shoaib <shoaibomaroway@gmail.com>
2021-04-24 15:16:54 +0200

    refactor predict file

--- predict.py
+++ predict.py
@@ -26,12 +26,12 @@ def process_image(image):
 
 
 def predict(img_path, f_model, f_top_k=5):
-    img = Image.open(img_path)
-    test_image = np.asarray(img)
-    processed_test_image = process_image(test_image)
-    final_image = np.expand_dims(processed_test_image, axis=0)
+    t_image = Image.open(img_path)
+    t_image = np.asarray(t_image)
+    processed_t_image = process_image(t_image)
+    f_image = np.expand_dims(processed_t_image, axis=0)
 
-    prediction = f_model.predict(final_image)
+    prediction = f_model.predict(f_image)
     prob = - np.partition(-prediction[0], f_top_k)[:f_top_k]
     class_pred = np.argpartition(-prediction[0], f_top_k)[:f_top_k]
 
@@ -39,28 +39,18 @@ def predict(img_path, f_model, f_top_k=5):
 
 
 if __name__ == '__main__':
-    print('predict.py, running')
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('arg1')
-    parser.add_argument('arg2')
+    parser.add_argument('--image_path')
+    parser.add_argument('--model_path')
     parser.add_argument('--top_k', type=int)
     parser.add_argument('--category_names')
 
     args = parser.parse_args()
-    print(args)
 
-    print('arg1:', args.arg1)
-    print('arg2:', args.arg2)
-    print('top_k:', args.top_k)
-    print('category_names:', args.category_names)
+    image_path = args.image_path
 
-    image_path = args.arg1
-
-    model = tf.keras.models.load_model(args.arg2, custom_objects={'KerasLayer': hub.KerasLayer})
+    model = tf.keras.models.load_model(args.model_path, custom_objects={'KerasLayer': hub.KerasLayer})
     top_k = args.top_k
-    if top_k is None:
-        top_k = 5
 
     probs, classes = predict(image_path, model, top_k)
 
@@ -70,7 +60,5 @@ if __name__ == '__main__':
         keys = [str(x + 1) for x in list(classes)]
         classes = [class_names.get(key) for key in keys]
 
-    print('======================================================================')
-    print('Probabilities:    ', probs)
-    print('Classes:          ', classes)
-    print('======================================================================')
+    print('Probabilities: ', probs)
+    print('Classes:       ', classes)
